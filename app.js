0707aeaae10bf85b17aa822e27a36484f72b407b
Meike <meike_hankewicz@yahoo.de>
2018-03-26 23:15:37 +0200

    working signup

--- app.js
+++ app.js
@@ -36,7 +36,34 @@ db.connect((err) => {
   console.log('MySQL is connected');
 });
 
-// Passport Authentication Strategy
+// Passport Authentication Strategies
+//Local Signup
+passport.use('local-signup', new LocalStrategy({usernameField: 'email', passReqToCallback: true},
+  function(req, email, password, done){
+    let sql = `SELECT email, name FROM users WHERE email = '${email}' OR name = '${req.body.username}'`;
+    db.query(sql, (err, user) => {
+      if(err) return done(err);
+      if(user.length == 0){
+        var newUser = {
+          username: req.body.username,
+          password: password
+        };
+
+        let sql = `INSERT INTO users (name, email, password) VALUES ('${req.body.username}', '${email}', '${password}')`;
+        db.query(sql, (err, user) => {
+          newUser.ID = user.insertId;
+          return done(null, newUser);
+        })
+      } else if(user[0].email == email){
+        return done(null, false, {message: 'This email is already signed up!'});
+      } else if(user[0].name == req.body.username){
+        return done(null, false, {message: 'Sorry, this username is already taken!'});
+      }
+    })
+  }
+));
+
+// Local Login
 passport.use('local-login', new LocalStrategy({passReqToCallback: true},
   function(req, username, password, done){
     let sql = `SELECT * FROM users WHERE name = '${username}'`;
@@ -75,6 +102,15 @@ app.get('/', (req, res) => {
   res.redirect('/login');
 });
 
+// Signup
+app.get('/signup', (req, res) => {
+  res.render('signup', {message: req.flash('error')})
+});
+
+app.post('/signup',
+  passport.authenticate('local-signup', {successRedirect: '/overview', failureRedirect: '/signup', failureFlash: true})
+);
+
 // Login
 app.get('/login', (req, res) => {
   res.render('login', {message: req.flash('error')});
@@ -83,6 +119,12 @@ app.get('/login', (req, res) => {
 app.post('/login', 
   passport.authenticate('local-login', {successRedirect: '/overview', failureRedirect: '/login', failureFlash: true})
 );
+
+// Logout
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/login');
+});
 
 // Skill Overview
 app.get('/overview', (req, res) => {
